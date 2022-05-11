b03213fc824bf49c3f888c8ace6fb73c8389031c
m.sharif <m@14003568>
2022-05-11 08:36:44 +0430

    Change refundReport

--- pages/refund-report.vue
+++ pages/refund-report.vue
@@ -219,6 +219,7 @@ export default {
         { text: this.$t('report.refundReport.headers.state'), value: 'state', sortable: false },
         { text: this.$t('report.refundReport.headers.requestId'), value: 'requestId', sortable: false },
         { text: this.$t('report.refundReport.headers.refundOrFailTime'), value: 'refundOrFailTime', sortable: false },
+        { text: this.$t('report.refundReport.headers.errorCode'), value: 'errorCode', sortable: false },
         { text: this.$t('report.transactionReport.headers.detail'), value: 'detail', sortable: false }
 
       ],
@@ -227,8 +228,7 @@ export default {
         { text: this.$t('report.refundReport.headers.transactionId'), value: 'transactionId', sortable: false },
         { text: this.$t('report.refundReport.headers.url'), value: 'url', sortable: false },
         { text: this.$t('report.refundReport.headers.ip'), value: 'ip', sortable: false },
-        { text: this.$t('report.refundReport.headers.switchResponseRrn'), value: 'switchResponseRrn', sortable: false },
-        { text: this.$t('report.refundReport.headers.errorCode'), value: 'errorCode', sortable: false }
+        { text: this.$t('report.refundReport.headers.switchResponseRrn'), value: 'switchResponseRrn', sortable: false }
       ],
 
       items: [],
