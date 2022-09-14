ded368d75e7472a03c94c5b77979a5c52197f282
m.roshandelpoor <m.roshandelpoor@asredanesh.com>
2022-09-14 17:46:13 +0430

    02- bug fix for logout

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,45 @@
 <template>
-  <small @click="redirect()">REDIRECT (click)</small>
+  <v-container fill-height fluid @click="redirect()">
+    <v-row align="center" justify="center">
+      <v-col md="12" style="text-align: center">
+        <v-progress-circular
+          indeterminate
+          color="green"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
 <script>
 export default {
   // this page only redirect to custom root page
   name: 'RedirectPage',
-  created () {
+  updated () {
     this.redirect()
   },
   mounted () {
     this.redirect()
   },
+  created () {
+    this.redirect()
+  },
   methods: {
     redirect () {
-      if (window.history.length > 2) {
-        this.$router.back()
-      }
+      setTimeout(() => {
+        if (window.history.length > 2) {
+          return this.$router.back()
+        }
 
-      this.$router.push('/transactionReport')
+        return this.$router.push('/transactionReport')
+      }, 2000)
     }
   }
 }
 </script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
