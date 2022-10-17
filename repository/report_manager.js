6cf682ffc1b3f58edb11a569ded8b4205d2de685
m.roshandelpoor <m.roshandelpoor@asredanesh.com>
2022-10-17 10:35:53 +0330

    05- add two new contract and change in all page to show these

--- repository/report_manager.js
+++ repository/report_manager.js
@@ -98,6 +98,12 @@ async function errorList (request, axios) {
   return rest
 }
 
+async function chartErrorCount (request, axios) {
+  const rest = await axios.post('error-report/chart-error-count', request)
+  console.log(rest)
+  return rest
+}
+
 async function operatorActivity (request, axios) {
   return await axios.post('report-statistic/operator-activity', request)
 }
@@ -552,6 +558,7 @@ export default {
   transactionDetailsQuery,
   operationList,
   errorList,
+  chartErrorCount,
   errorCodeList,
   smsReport,
   downloadSmsReport,
