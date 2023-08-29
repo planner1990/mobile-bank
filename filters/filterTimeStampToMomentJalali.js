b0716fdbab3b7ff312a0edba541a2a3445158c11
m.roshandelpoor <m.roshandelpoor@asredanesh.com>
2023-08-29 17:19:06 +0330

    add sum in transfer page under grid

--- filters/filterTimeStampToMomentJalali.js
+++ filters/filterTimeStampToMomentJalali.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import momentJalali from 'moment-jalaali'
+import moment from 'moment-jalaali'
 
 export default () => {
   Vue.filter('filterTimeStampToMomentJalali', (date, format = 'HH:mm jYYYY/jM/jD') => {
     if (date != null) {
-      date = new Date(date)
-      return momentJalali(date).format(format)
+      return moment(date).format('HH:mm:ss jYYYY/jM/jD')
     }
   })
 }
